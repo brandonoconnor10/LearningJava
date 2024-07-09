2fbb1be322f8bcc5fb811f9a2b2b5bc1ab27b18d
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-09 13:54:58 +0200

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -1,8 +1,13 @@
 public class Variables {
 
     public static void main(String[] args) {
-
+        
     }
-
-    
+    class Example{
+        static int a = 1; //static variable  
+        int data = 99; //instance variable  
+        void method() {
+            int b = 90; //local variable  
+        }
+    }
 }
