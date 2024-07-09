8bcaf80a50c4a26893f9233683ba81548273ecd7
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-09 13:53:27 +0200

    Variables Summary

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+
+    public static void main(String[] args) {
+    // Variable Declaration Example
+    int count;
+
+    // Variable Initialization Example
+    count = 100;
+
+    // You can combine both into one statement
+    int count = 100;
+    }
+
+    
+}
