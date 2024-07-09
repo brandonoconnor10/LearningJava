b4d1fc78054c0d00fc1de1462b15ac3bec5bf46d
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-09 13:57:31 +0200

    Type Conversion and Casting

--- Variables.java
+++ Variables.java
@@ -1,13 +1,9 @@
 public class Variables {
 
     public static void main(String[] args) {
-        
-    }
-    class Example{
-        static int a = 1; //static variable  
-        int data = 99; //instance variable  
-        void method() {
-            int b = 90; //local variable  
-        }
+        // Auto Type Conversion
+        double d;
+        int i = 10;
+        d = i;
     }
 }
