573ec912dda1d4462e98147465df1ba7c9318de8
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-09 16:07:21 +0200

    Update JavaArrays.java

--- JavaArrays.java
+++ JavaArrays.java
@@ -1,24 +1,4 @@
 public class JavaArrays {
-    // Declaring your Array
-    int intArray [];
+    // Array of Objects Syntax: Class obj[] = new Class [array_length];
 
-    // Constructing an Array
-    intArray = new int[10]; // Defines that intArray will store 10  integer values
-
-    // Declaration and Construction combined
-    int intArray [] = new int [10];
-
-    // Initialize an Array
-    intArray[0] = 1; // Assigns an integer value 1 to the first element 0 of the array
-
-    // Declaring and initialize an Array
-    [] = {};
-    // Example
-    int intArray[] = {1, 2, 3, 4};
-    // Initializes an integer array of length 4 where the first element is 1, second element is 2 and so on
-
-
-    // Multidimensional Arrays
-
-    int twoD [ ] [ ] = new int [4][5]; 
 }
