0b93b1f58eb84a79c69673c406514a81a98f54e8
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-10 16:18:11 +0200

    Create JavaException.java

---
+++ JavaException.java
@@ -0,0 +1,14 @@
+class JavaException {
+    public static void main(String args[]){
+      int d = 0;
+      int n = 20;
+      try{
+         int fraction = n/d;
+         System.out.println("End Of Main");
+      }
+      catch(ArithmeticException e){
+         System.out.println("In the catch Block due to Exception = " + e);
+      }
+      System.out.println("End of Main");
+    }
+ }
