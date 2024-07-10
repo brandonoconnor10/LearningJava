f7620d9d445c53bbd40fe067dce0f1b2fc12797c
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-10 16:19:54 +0200

    Update JavaException.java

--- JavaException.java
+++ JavaException.java
@@ -6,7 +6,7 @@ class JavaException {
          int fraction = n/d;
          System.out.println("End Of Main");
       }
-      catch(ArithmeticException e){
+      catch(Exception e){
          System.out.println("In the catch Block due to Exception = " + e);
       }
       System.out.println("End of Main");
